f053b70ab5f479bf4ebc31ee3b3a63fe802f8433
dresenhista <andresakarine@gmail.com>
2019-05-10 16:45:44 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# google_my_business
---
+++ sample.py
@@ -0,0 +1,81 @@
+import sys	
+import json
+
+from pprint import pprint
+
+from googleapiclient import sample_tools
+from googleapiclient.http import build_http
+
+import pandas as pd
+
+discovery_doc = "myBusiness_discovery.json"
+
+# GET https://mybusiness.googleapis.com/v4/accounts/account_name/locations/location_name/reviews
+
+def flattenjson( b, delim="__" ):
+    val = {}
+    for i in b.keys():
+        if isinstance( b[i], dict ):
+            get = flattenjson( b[i], delim )
+            for j in get.keys():
+                val[ i + delim + j ] = get[j]
+        else:
+            val[i] = b[i]
+
+    return val
+
+def flatlist(items):
+    return [flattenjson(item) for item in items]
+
+def location():
+    # Use the discovery doc to build a service that we can use to make
+    # MyBusiness API calls, and authenticate the user so we can access their
+    # account
+    service, flags = sample_tools.init(['sample.py'], "mybusiness", "v4", __doc__, __file__, scope="https://www.googleapis.com/auth/business.manage", discovery_filename=discovery_doc)
+    output = service.accounts().list().execute()
+
+    # print("List of Accounts:\n")
+    # print(json.dumps(output, indent=2) + "\n")
+
+    firstAccount = output["accounts"][0]["name"]
+    # Get the list of locations for the first account in the list
+    # print("List of Locations for Account " + firstAccount)
+    locationsList = service.accounts().locations().list(parent=firstAccount).execute()
+    firstLocation = locationsList["locations"][0]["name"]
+    return firstLocation  
+
+def reviews(location):
+    # Use the discovery doc to build a service that we can use to make
+    # MyBusiness API calls, and authenticate the user so we can access their
+    # account
+    service, flags = sample_tools.init(['sample.py'], "mybusiness", "v4", __doc__, __file__, scope="https://www.googleapis.com/auth/business.manage", discovery_filename=discovery_doc)
+
+    reviewsApi = service.accounts().locations().reviews()
+    request = reviewsApi.list(parent=location)
+    #reviewsList = request.execute()
+    reviews = []
+
+    # pagination
+    # https://developers.google.com/api-client-library/python/guide/pagination
+    while request is not None:
+      response = request.execute()
+
+      # Do something with the activities
+      reviews += response["reviews"]
+
+      request = reviewsApi.list_next(request, response) # all pages
+      # request = None # first page only
+
+    print(len(reviews))
+    return reviews
+
+def export_csv(reviews_dict, dest='reviews.csv'):
+    df = pd.DataFrame(flatlist(reviews_dict))
+    output = open(dest, 'w', encoding="utf-8")
+    output.write(df.to_csv())
+
+def main(argv):
+    export_csv(reviews(location()))
+    
+if __name__ == "__main__":
+  main(sys.argv)
